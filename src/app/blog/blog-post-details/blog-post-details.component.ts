7ed06cd4710d37c744c19a446f0a1d76afd1e698
preferredMS727 <massimiliano.soremic4@gmail.com>
2020-10-08 08:00:16 +0200

    blog details

--- src/app/blog/blog-post-details/blog-post-details.component.ts
+++ src/app/blog/blog-post-details/blog-post-details.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { ButterCMSService } from '../../_services'
+import { map, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-blog-post-details',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogPostDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    protected route: ActivatedRoute) {
+  }
 
-  ngOnInit(): void {
+  protected slug$: Observable<string>;
+  public post = {
+    meta: null,
+    data: null
+  };
+
+  ngOnInit() {
+    this.slug$ = this.route.paramMap
+      .pipe(
+        map(params => (params.get('slug')))
+      );
+
+    this.slug$.pipe(
+      take(1))
+      .subscribe(slug => {
+        ButterCMSService.post.retrieve(slug)
+          .then((res) => {
+            this.post = res.data;
+          }).catch((res) => {
+            console.log(res);
+          });
+      });
   }
 
 }
